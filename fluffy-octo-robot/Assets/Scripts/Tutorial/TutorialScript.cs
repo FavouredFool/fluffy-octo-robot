98e0db5eb3cf5f7cb0185349268524fe2d460cd4
Tim Wahrburg <t.wahrburg@gmail.com>
2022-01-23 19:08:03 +0100

    Added TutorialVideos

--- fluffy-octo-robot/Assets/Scripts/Tutorial/TutorialScript.cs
+++ fluffy-octo-robot/Assets/Scripts/Tutorial/TutorialScript.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class TutorialScript : MonoBehaviour
 {
 
     public GameObject[] scenes;
+    public VideoPlayer[] videos;
     public Text buttonText;
     int counter = 0;
+    int videoCounter = 0;
 
     protected void Update()
     {
@@ -27,9 +30,17 @@ public class TutorialScript : MonoBehaviour
     {
         if (counter < scenes.Length-1)
         {
+            Debug.Log(videoCounter);
+            videos[Mathf.Max(0, videoCounter-1)].gameObject.SetActive(false);
             scenes[counter].SetActive(false);
             counter++;
+            if (scenes[counter].GetComponent<RawImage>())
+            {
+                videos[videoCounter].gameObject.SetActive(true);
+                videoCounter++;
+            }
             scenes[counter].SetActive(true);
+
         } else
         {
             SceneManager.LoadScene(sceneName: "MainMenu");
